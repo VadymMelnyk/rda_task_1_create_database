8bdc27bb98130c2ed1700def0c1602081c5a4b1b
Vadym <vadym@example.com>
2026-09-10 17:55:44 +0300

    Solution2

--- task.sql
+++ task.sql
@@ -18,15 +18,15 @@ CREATE TABLE Customers(
 );
 CREATE TABLE Orders(
     ID INT AUTO_INCREMENT,
-    CustomerID INT,
+    CustomerID INT NULL ,
     Date DATE,
     PRIMARY KEY (ID),
     FOREIGN KEY (CustomerID) REFERENCES Customers(ID) ON DELETE SET NULL
 );
 CREATE TABLE OrderItems(
     ID INT AUTO_INCREMENT,
-    OrderID INT,
-    ProductID INT,
+    OrderID INT NULL,
+    ProductID INT NULL,
     PRIMARY KEY (ID),
     FOREIGN KEY (OrderID) REFERENCES Orders(ID) ON DELETE SET NULL,
     FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE SET NULL
